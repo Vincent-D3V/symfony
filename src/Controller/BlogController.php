4cbc3cdb8d8d2b860a4fbed32b1cb2a1db10c9e2
Vincent Mallard <vincent.mallard@gmail.com>
2019-05-16 11:13:11 +0200

    Finished Symfony Quest Number 8 :tada:

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,112 +1,74 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: wilder
- * Date: 14/05/19
- * Time: 09:31
- */
-
 namespace App\Controller;
-
-use App\Entity\Category;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Article;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Category;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
     /**
-     * Show all row from article's entity
-     *
-     * @Route("/", name="index")
-     * @return Response A response instance
+     * @Route("/index", name="blog_index")
      */
-    public function index(): Response
+    public function index()
     {
         $articles = $this->getDoctrine()
             ->getRepository(Article::class)
             ->findAll();
-
-        if (!$articles) {
+        if (!$articles){
             throw $this->createNotFoundException(
-                'No article found in article\'s table.'
+                "No articles found in article's table."
             );
         }
-
-        return $this->render(
-            'blog/index.html.twig',
-            ['articles' => $articles]
-        );
+        return $this->render('blog/index.html.twig', [
+            'owner' => 'Edouard',
+            'articles' => $articles,
+        ]);
     }
-
     /**
-     * Getting a article with a formatted slug for title
+    @param string $slug The slugger
      *
-     * @param string $slug The slugger
-     *
-     * @Route("/{slug<^[a-z0-9-]+$>}",
+     * @Route("/blog/{slug<^[a-z0-9-]+$>}",
      *     defaults={"slug" = null},
      *     name="blog_show")
-     * @return Response A response instance
+     *  @return Response A response instance
      */
-    public function show(?string $slug): Response
+    public function show(string $slug ="article sans titre"): Response
     {
-        if (!$slug) {
-            throw $this
-                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
+        if (!$slug)
+        {
+            throw $this->createNotFoundException('No slug has been sent to find an article in article\'s table .');
         }
-
         $slug = preg_replace(
             '/-/',
             ' ', ucwords(trim(strip_tags($slug)), "-")
         );
-
         $article = $this->getDoctrine()
             ->getRepository(Article::class)
-            ->findOneBy(['title' => mb_strtolower($slug)]);
-
-        if (!$article) {
+            ->findOneBy(['title'=> mb_strtolower($slug)]);
+        if (!$article)
+        {
             throw $this->createNotFoundException(
-                'No article with ' . $slug . ' title, found in article\'s table.'
+                'No article with '.$slug.' title found in article\'s table.'
             );
         }
-
-        return $this->render(
-            'blog/show.html.twig',
+        return $this->render('blog/blog.html.twig',
             [
+                'title' => ucwords(str_replace('-',' ', $slug)),
                 'article' => $article,
                 'slug' => $slug,
-            ]
-        );
+            ]);
     }
     /**
-     * @Route("/category/{categoryName}", name="show_category")
+     * @Route("/category/{name}", name="show_category").
      */
-    public function showByCategory(string $categoryName): Response
+    public function showByCategory(Category $category): Response
     {
-        // récuperer une category avec findOneBy
-        $category = $this->getDoctrine()
-        ->getRepository(Category::class)
-        ->findOneBy(['name' => $categoryName]);
-
-        $article = $this->getDoctrine()
-            ->getRepository(Article::class)
-            ->findBy(['category' => $category], array('id' => 'desc'), 3);        // Tri
-        return $this->render(
-            'blog/category.html.twig',
+        return $this->render('blog/category.html.twig',
             [
-                'article' => $article,
+                'articles' => $category->getArticles(),
                 'category' => $category,
-
-            ]
-        );
-
-        // récuperer les articles de cette categorie avec findBy
-        // limite de 3 articles et un tri par id décroissant
-
-        //retourner dans une vue
-
-
+            ]);
     }
 }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace App\Entity;
-
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
  */
@@ -15,26 +14,63 @@ class Category
      * @ORM\Column(type="integer")
      */
     private $id;
-
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=100)
      */
     private $name;
-
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Article", mappedBy="category")
+     */
+    private $articles;
     public function getId(): ?int
     {
         return $this->id;
     }
-
     public function getName(): ?string
     {
         return $this->name;
     }
-
     public function setName(string $name): self
     {
         $this->name = $name;
-
+        return $this;
+    }
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
+    /**
+     * @return Collection|Article[]
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+    /**
+     * param Article $article
+     * @return Category
+     */
+    public function addArticle(Article $article): self
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles[] = $article;
+            $article->setCategory($this);
+        }
+        return $this;
+    }
+    /**
+     * @param Article $article
+     * @return Category
+     */
+    public function removeArticle(Article $article): self
+    {
+        if ($this->articles->contains($article)) {
+            $this->articles->removeElement($article);
+            // set the owning side to null (unless already changed)
+            if ($article->getCategory() === $this) {
+                $article->setCategory(null);
+            }
+        }
         return $this;
     }
 }
